2f7f21ffff371e1099538c8a8edb80b1e0b980b5
Ualfinhe Barbosa <147889889+hgfxxx53@users.noreply.github.com>
2024-12-01 18:18:50 +0000

    Update Carro.java

--- src/main/java/com/alfinhe/Carro.java
+++ src/main/java/com/alfinhe/Carro.java
@@ -1,4 +1,15 @@
 package com.alfinhe;
 
 public class Carro {
+    public static void main(String[] args) {
+        Objeto carro = new Objeto();
+        carro.setCarro("Ford");
+        carro.setMtricula("ABC-1234");
+        carro.setFabrica("Ford");
+        carro.setModelo("Fiesta");
+        System.out.println(carro);
+
+    }
+
+
 }
